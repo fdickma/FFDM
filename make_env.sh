5bd391a429955066d4ff208c8da25a4d3357fee7
Frank <fdickma@t-online.de>
2025-09-27 19:07:04 +0200

    Added script to generate python environment

---
+++ make_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python -m venv env
+env/bin/pip install yfinance sqlalchemy matplotlib scipy chardet flask flask_sqlalchemy flask_login bcrypt
+# env/bin/pip install --upgrade *
+env/bin/pip freeze > requirements.txt
+env/bin/pip install --upgrade -r requirements.txt
